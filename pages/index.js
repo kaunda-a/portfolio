f5908e1c896e593b218bfbe0ea4c2cc7242f8dbc
Ndumiso Banda <67536043+dukeow8@users.noreply.github.com>
2021-10-26 02:15:55 -0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CQKNXG1RXS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CQKNXG1RXS');
+</script>
+        <title>My Projects | Home</title>
+        <meta name="keywords" content="project"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>NDUMISO BANDA</h1>
+        <p className={styles.text}>Programming graduate with proven skills, Strong creative ability, Strong communication skills,Attention to detail and a logic approach to work . Seeking a position as Web Developer at tech industries, to leverage organizational and research skills to support internal and external communication.</p>
+        <p className={styles.text}>Aspiring Web Developer with a passion for web application development and success in managing development projects in tech industries. Skilled in conceptualizing, designing, development, and deploying software containing logical and mathematical solutions to business problems. Dedicated to driving innovation with the ability to follow industry and technological trends, and facilitating early adoption of innovations. <a href="https://github.com/dukeow8/Store.git" target="_blank">Recent project</a> </p>
+        <Link href="/project/">
+          <a className={styles.btn}>See My projects</a>
+        </Link>
+      </div>
+    </>
+  )
+}
